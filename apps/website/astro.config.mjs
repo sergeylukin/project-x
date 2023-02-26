3a79ae1c2fb52be58f496c045e14cab3781b9fc5
Sergey Lukin <contact@sergeylukin.com>
2023-02-26 16:38:39 +0200

    use ~ to resolve website inner modules

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,9 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/website',
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
-
