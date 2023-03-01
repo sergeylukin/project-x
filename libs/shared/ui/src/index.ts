ab75b5482f1bd81ee863663867e07b1b0334f8cb
Sergey Lukin <contact@sergeylukin.com>
2023-03-01 16:58:52 +0200

    fix ref to types

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,5 +1,6 @@
 export { default as Footer } from './lib/Footer.astro';
 export { default as Header } from './lib/Header.astro';
+export { default as Card } from './lib/Card.astro';
 export { default as Logo } from './lib/Logo.astro';
 export { default as ToggleMenu } from './lib/ToggleMenu.astro';
 export { default as ToggleTheme } from './lib/ToggleTheme.astro';
