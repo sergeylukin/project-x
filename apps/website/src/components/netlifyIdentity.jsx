24fbba520c210ba52d4e6027c03c920366c1f757
Sergey Lukin <contact@sergeylukin.com>
2023-03-01 23:52:09 +0200

    o

--- apps/website/src/components/netlifyIdentity.jsx
+++ apps/website/src/components/netlifyIdentity.jsx
@@ -1,24 +1,30 @@
+import React, { useEffect } from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
 
-netlifyIdentity.on('init', (user) => {
-  console.log('INIT ANYWAYS');
-  if (!user) {
-    console.log('SETTING UP INIT');
-    netlifyIdentity.on('login', () => {
-      console.log('REDIRECTING');
-      document.location.href = '/admin/';
+export const NetlifyIdentity = () => {
+  useEffect(() => {
+    netlifyIdentity.on('init', (user) => {
+      console.log('INIT ANYWAYS');
+      if (!user) {
+        console.log('SETTING UP INIT');
+        netlifyIdentity.on('login', () => {
+          console.log('REDIRECTING');
+          document.location.href = '/admin/';
+        });
+      }
     });
-  }
-});
 
-netlifyIdentity.on('logout', () => console.log('Logged out'));
-netlifyIdentity.on('error', (err) => console.error('Error', err));
-netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
+    netlifyIdentity.on('logout', () => console.log('Logged out'));
+    netlifyIdentity.on('error', (err) => console.error('Error', err));
+    netlifyIdentity.on('open', () => console.log('Widget opened'));
+    netlifyIdentity.on('close', () => console.log('Widget closed'));
 
-netlifyIdentity.init({
-  // container: '#netlify-modal', // defaults to document.body
-  locale: 'en', // defaults to 'en'
-});
+    netlifyIdentity.init({
+      // container: '#netlify-modal', // defaults to document.body
+      locale: 'en', // defaults to 'en'
+    });
 
-netlifyIdentity.open(); // open the modal
+    netlifyIdentity.open(); // open the modal
+  }, []);
+  return <div />;
+};
