a8ee507511a89042edda13cb91068a04b6d80cef
Sergey Lukin <contact@sergeylukin.com>
2023-03-02 03:01:23 +0200

    wrapppping

--- apps/website/src/pages/admin.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const get: APIRoute = ({ params, request }) => {
-  return {
-    body: JSON.stringify({
-      path: new URL(request.url).pathname,
-    }),
-  };
-};
