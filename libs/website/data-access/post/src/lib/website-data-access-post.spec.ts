da5f251b5edc4013d771fc3fd44d52c207d735aa
Sergey Lukin <contact@sergeylukin.com>
2023-03-16 14:48:43 +0200

    remove data-access/post lib

--- libs/website/data-access/post/src/lib/website-data-access-post.spec.ts
+++
@@ -1,7 +0,0 @@
-import { websiteDataAccessPost } from './website-data-access-post';
-
-describe('websiteDataAccessPost', () => {
-  it('should work', () => {
-    expect(websiteDataAccessPost()).toEqual('website-data-access-post');
-  });
-});
