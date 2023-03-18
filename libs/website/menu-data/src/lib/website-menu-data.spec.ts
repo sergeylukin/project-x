32e95d0a4b9adb129b813aaba6b40654b2a6d31c
Sergey Lukin <contact@sergeylukin.com>
2023-03-18 17:25:11 +0200

    move menu data to app

--- libs/website/menu-data/src/lib/website-menu-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { websiteMenuData } from './website-menu-data';
-
-describe('websiteMenuData', () => {
-  it('should work', () => {
-    expect(websiteMenuData()).toEqual('website-menu-data');
-  });
-});
