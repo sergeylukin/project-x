4bc50790babff8370d643aadab808c1cf677669a
Sergey Lukin <contact@sergeylukin.com>
2023-03-18 14:20:38 +0200

    move url to util

--- libs/shared/util/url/jest.config.ts
+++ libs/shared/util/url/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'shared-utils-url',
+  displayName: 'shared-util-url',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/shared/utils/url',
+  coverageDirectory: '../../../../coverage/libs/shared/util/url',
 };
