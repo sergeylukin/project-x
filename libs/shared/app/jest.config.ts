71571697f7bb0639106461e98172e7fc9c131dea
Sergey Lukin <contact@sergeylukin.com>
2023-03-21 01:08:44 +0200

    move app to shared/app

--- libs/shared/app/jest.config.ts
+++ libs/shared/app/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'website-app',
+  displayName: 'shared-app',
   preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/website/app',
+  coverageDirectory: '../../../coverage/libs/shared/app',
 };
